ec0e2fa7d32f8d2f06cadca4869472ed8337655d
Donette May <donettemay@Donettes-MacBook-Air.local>
2025-10-28 22:42:13 -0400

    Day 5 homeowrk

---
+++ Day5-homework.js
@@ -0,0 +1,14 @@
+// Create a variable using let to hold your name
+let myName = "Donette";
+console.log("My name is:", myName);
+
+// Change the value of the let variable
+myName = "Andy";
+console.log("Updated name:", myName);
+
+// Create a constant for your favorite color
+const favoriteColor = "Gray";
+console.log("My favorite color is:", favoriteColor);
+
+// Try changing the const variable (this will cause an error)
+favoriteColor = "Green"; // ❌ TypeError: Assignment to constant variable
